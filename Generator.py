5e98372cbadeb02fed58af551acb662ea807e339
jnthn-b <JayJay150902@gmail.com>
2018-04-08 20:03:12 +0200

    Create Generator.py

---
+++ Generator.py
@@ -0,0 +1,15 @@
+import random
+print('This is a free Project by Jonathan (Jnthn) licenced under the MIT License.')
+amount = input('Enter the Amount of Junk Blocks: ')
+count = 0
+
+while int(float(amount)) > count:
+    randomint = random.randint(1,99)
+    if randomint <= 9:
+        randomint = str(randomint)
+        randomint = '0' + randomint
+    print('__asm _emit 0x' + str(randomint) + ' \\')
+    count = count + 1
+
+print("done!")
+input()
